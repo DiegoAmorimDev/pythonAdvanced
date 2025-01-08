9ca6d161198765ec8ba61466443221f67a06b903
Diego Amorim <diegogpamorim1305@gmail.com>
2025-01-08 10:14:22 -0300

    fourth commit

---
+++ estudosQuestions.py
@@ -0,0 +1,9 @@
+soma = 0
+while True:
+    n = float(input('Informe um número: '))
+    if n < 0:
+        continue
+    if n == 0:
+        break
+    soma = soma + n
+print('Soma dos números:', soma)
