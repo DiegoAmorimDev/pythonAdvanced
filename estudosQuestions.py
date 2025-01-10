bd042ff826649610c44d213efc7eabf3834e527f
Diego Amorim <diegogpamorim1305@gmail.com>
2025-01-10 14:45:07 -0300

    atualizações

--- estudosQuestions.py
+++
@@ -1,9 +0,0 @@
-soma = 0
-while True:
-    n = float(input('Informe um número: '))
-    if n < 0:
-        continue
-    if n == 0:
-        break
-    soma = soma + n
-print('Soma dos números:', soma)
---
+++ testes.py
@@ -0,0 +1,22 @@
+lista_candidatos = {}
+
+while True:
+    print(f'digite os dados do candidato (ou digite sair para encerrar)')
+    nome = input('nome do candidato: ')
+    if nome.lower() == 'sair':
+        break
+
+    cod = input('digite o numero de dois digitos do candidato: {nome} ')
+
+    if cod.isdigit():
+        cod = int(cod)
+        lista_candidatos[nome] = cod
+    else:
+        print('código inválido:')
+
+    print('candidato {nome} de código {cod} adicionado')
+
+print(f'candidatos cadastrados: ')
+for nome, cod in lista_candidatos.items():
+    
+    print(f'nome: {nome}, codigo: {cod}')
